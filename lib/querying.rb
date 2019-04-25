8fcaaf6ef5d06c13a490a3ec6d31a319fe4ca409
lieby weintraub <liebahinda@gmail.com>
2019-04-25 20:16:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT Characters.species"
+  "SELECT Characters.species COUNT(species) FROM Characters"
 end
 
 def select_name_and_series_subgenres_of_authors
