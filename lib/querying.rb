b78906fe33e51922c26d8f8567400c52b0f1228d
lieby weintraub <liebahinda@gmail.com>
2019-04-25 20:04:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT Character.name, Character.moto ORDER BY LENGHT"
+  "SELECT Characters.name, Characters.moto FROM characters ORDER BY LENGHT"
 end
 
 
